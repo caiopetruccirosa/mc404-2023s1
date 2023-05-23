7c06f24dcaa9896c8b7fcb1e964c61b5aa6d7024
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2023-05-23 15:10:37 -0300

    adiciona mais linhas nos digitos da atividade01

--- atividade01/a1.s
+++ atividade01/a1.s
@@ -1,22 +1,21 @@
 # RA: 248245
 # Nome: Caio Petrucci dos Santos Rosa
 
-# Revisar coisa de estar piscando
-# Salvar registradores nas funcoes que tem ecall
-
 .data
 # Digitos do RA descritos por linhas:
-# Os digitos ocupam 24 bits (4 bits por digito e 6 digitos no total)
-# e existe 1 bit zerado entre cada digito (5 bits no total). Adicionalmente,
-# existe um padding de 2 bits zerados na esquerda e 1 bit zerado na direita.
-# Dessa forma, cada linha é representada por 32 bits no total.
+#  Os digitos ocupam 24 bits (4 bits por digito e 6 digitos no total)
+#  e existe 1 bit zerado entre cada digito (5 bits no total). Adicionalmente,
+#  existe um padding de 2 bits zerados na esquerda e 1 bit zerado na direita.
+#  Dessa forma, cada linha é representada por 32 bits no total.
 digitos_ra:
     .word 0b00011000001001100011000001011110
     .word 0b00100100011010010100100011010000
     .word 0b00100100101010010100100101010000
+    .word 0b00000101001010010000101001010000
     .word 0b00000101001001100000101001011100
-    .word 0b00001001111001100001001111000010
+    .word 0b00001001111010010001001111000010
     .word 0b00010000001010010010000001000010
+    .word 0b00100000001010010100000001000010
     .word 0b00100000001010010100000001000010
     .word 0b00111100001001100111100001011100
 
@@ -64,7 +63,9 @@ fim_main:
 # Funcoes Auxiliares
 ####################################
 
-# Funcao Pausa que executa um laço contendo nop por N vezes (N = a0)
+# Funcao Pausa:
+#  Executa um laço, contendo nop, por N vezes. 
+#  O parametro N eh recebido em a0.
 Pausa:
 loop_pausa:
     beq a0, zero, fim_pausa
@@ -74,10 +75,10 @@ loop_pausa:
 fim_pausa:
     ret
 
-# Funcao MostrarDigitos que mostra na tela os digitos armazenados 
-# na memoria de acordo com a iteracao/posicao que a animacao esta.
-# Recebe o endereco de memoria do vetor com os digitos em a0 e a iteracao 
-# atual em a1 (deve ser um numero entre 0 e 47, possibilitando 48 iteracoes)
+# Funcao MostrarDigitos: 
+#  Mostra na tela os digitos armazenados na memoria de acordo com a iteracao/posicao 
+#  que a animacao esta. Recebe o endereco de memoria do vetor com os digitos em a0 e 
+#  a iteracao atual em a1 (deve ser um numero entre 0 e 47, possibilitando 48 iteracoes).
 MostrarDigitos:
     # Armazena os registradores na pilha, pois a funcao nao eh folha ja que possui uma chamada ecall
     addi sp, sp, -20
@@ -91,8 +92,8 @@ MostrarDigitos:
     mv s0, a0
     mv s1, a1
 
-    # 8 linhas para imprimir
-    li s2, 8
+    # 10 linhas para imprimir
+    li s2, 10
 loop_linhas:
     # Itera para cada linha
     beq s2, zero, fim_mostrar
@@ -113,9 +114,9 @@ shift_esquerda:
 fim_shift:
 
     # Pinta os digitos na tela da linha atual
-    # Os digitos serao pintados na tela das linhas 2 a 10
+    # Os digitos serao pintados na tela das linhas 1 a 10
     li a0, 0x110
-    addi a1, s2, 1
+    mv a1, s2
     mv a2, s3
     ecall
 
